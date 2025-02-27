18e57758441ff1a64d37568f152867c9da3baf0b
valeedkhanjadoon <70755199+valeedkhanjadoon@users.noreply.github.com>
2025-02-27 21:35:33 +0500

    Pointer to Structure

--- basics/pointer_to_structure.cpp
+++ basics/pointer_to_structure.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+// Pointer (pointing) to Structure
+
+struct Rectangle {
+    int length;
+    int breath;
+};
+
 int main() {
+    Rectangle r = {10, 5};
+    Rectangle *p = &r;
+
+    cout<<p->length<<endl;
+    cout<<p->breath<<endl;
+
+    // Structure in HEAP
+    Rectangle *k;
+    // k = (struct Rectangle *)malloc(sizeof(struct Rectangle)); // In C Programming Language
+    k = new Rectangle; // In C++ Programming Language
+
+    k->length = 7;
+    k->breath = 15;
+
+    cout << k->length << endl;
+    cout << k->breath << endl;
 
     return 0;
 }
