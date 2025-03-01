7c899174b670c11f6acd7e22adceb9048d95dbb1
valeedkhanjadoon <70755199+valeedkhanjadoon@users.noreply.github.com>
2025-03-01 21:37:22 +0500

    Parameter Passing methods

--- basics/parameter_passing_methods.cpp
+++ basics/parameter_passing_methods.cpp
@@ -18,13 +18,24 @@ void swapByAddress(int *x, int *y) {
     *y = temp;
 }
 
+// In C++, reference is nickname of a variable, so it should directly represent that variable.
+void swapByReference(int &x, int &y) {
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
 int main() {
     int a, b;
     a = 10;
     b = 10;
     cout << "Before: " << a << " " << b << endl;
+    
     swapByValue(a, b);
     swapByAddress(&a, &b);
+    swapByReference(a, b);
+    
     cout << "After: " << a << " " << b << endl;
 
     return 0;
